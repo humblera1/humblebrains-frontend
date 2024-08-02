992332a7c78a43e5c8776105da63f041a4d5a80e
Maxim Koshkin <humblerat@mail.ru>
2024-08-02 17:01:19 +0700

    init project

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    modules: ["@nuxt/eslint"],
+    components: [
+        {path: '~/shared/ui', prefix: 'Ui', extensions: ['.vue']},
+        {path: '~/shared/icons', prefix: 'Icon'},
+        {path: '~/features', prefix: 'Feature', extensions: ['.ts']},
+        {path: '~/widgets', prefix: 'Widget', extensions: ['.vue']},
+    ],
+    runtimeConfig: {
+        public: {
+            api: process.env.API_URL ?? '',
+        },
+    },
+})
