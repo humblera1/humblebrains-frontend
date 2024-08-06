07c88ae99f7457a91dfb5b4e32488383bc96ebe4
Maxim Koshkin <humblerat@mail.ru>
2024-08-06 13:54:55 +0700

    added color-mode component (beta)

--- widgets/color-mode/index.vue
+++ widgets/color-mode/index.vue
@@ -2,12 +2,12 @@
     <ClientOnly>
         <div :class="['color-mode', isDark ? 'color-mode_dark' : '']" @click="isDark = !isDark">
             <div class="color-mode__circle">
-                <IconColorModeSun />
+                <IconColorModeMoon :class="isDark ? 'color-mode_visible' : 'color-mode_hidden'" />
+                <IconColorModeSun :class="isDark ? 'color-mode_hidden' : 'color-mode_visible'" />
             </div>
-            <p class="color-mode__label">{{ isDark ? 'Darkmode' : 'Daymode' }}</p>
+            <p class="color-mode__label" :class="isDark ? 'color-mode__label_dark' : ''">{{ isDark ? 'Darkmode': 'Daymode' }}</p>
         </div>
     </ClientOnly>
-    {{ colorMode }}
 </template>
 
 <script setup lang="ts">
@@ -23,45 +23,4 @@ const isDark = computed({
 });
 </script>
 
-<style scoped lang="scss">
-.color-mode {
-    position: relative;
-    cursor: pointer;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 16px;
-    min-width: 155px;
-    width: 155px;
-    height: 50px;
-    border-radius: 100px;
-    padding: 5px;
-    background-color: var(--blue-badge);
-
-    &_dark {
-        .color-mode__circle {
-            transform: translateX(calc(150px - 40px - 5px));
-        }
-    }
-
-    svg {
-        color: var(--white-to-black);
-    }
-
-    &__circle {
-        position: absolute;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 40px;
-        height: 40px;
-        border-radius: 100%;
-        background-color: var(--blue-secondary);
-    }
-
-    &__label {
-        @include mainFont(400, 16, var(--text-gray-to-light));
-    }
-}
-
-</style>
+<style scoped lang="scss" src="./color-mode.styles.scss"></style>
