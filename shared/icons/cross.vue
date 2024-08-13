9b994aaffcc4623f04406231a3adfe6e14e263d2
Maxim Koshkin <humblerat@mail.ru>
2024-08-13 20:22:20 +0700

    init modal component

--- shared/icons/cross.vue
+++ shared/icons/cross.vue
@@ -2,7 +2,7 @@
     <svg width="12" height="12" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
             d="M0.758057 12.757L6.75806 6.75699L12.7581 12.757M12.7581 0.756989L6.75691 6.75699L0.758057 0.756989"
-            stroke="#6771A2"
+            stroke="currentColor"
             stroke-width="1.5"
             stroke-linecap="round"
             stroke-linejoin="round"
---
+++ shared/ui/modal/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-show="model" class="modal__backdrop" @click="closeModal">
+        <div class="modal">
+            <div class="modal__inner" @click.stop>
+                <button v-if="!withoutCloseBtn" class="modal__close" @click="closeModal"><IconCross /></button>
+                <div class="modal__content">
+                    <slot />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { Modal } from '~/shared/ui/modal/modal.types';
+
+const model = defineModel<boolean>();
+
+withDefaults(defineProps<Modal>(), {
+        withoutCloseBtn: false,
+    },
+);
+
+const closeModal = () => {
+    model.value = false;
+}
+</script>
+
+<style scoped lang="scss" src="./modal.styles.scss"></style>
