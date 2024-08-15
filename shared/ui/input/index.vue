c15bc806fdeb16bb6c2211a38f2b0a2e86932959
Maxim Koshkin <humblerat@mail.ru>
2024-08-15 21:56:26 +0700

    init auth modal

--- shared/ui/input/index.vue
+++ shared/ui/input/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <div :class="['input', 'input_' + theme, { 'input_invalid': hasError }]">
+    <div :class="['input', 'input_' + theme, { input_invalid: hasError }]">
         <div class="input__wrapper">
-            <label :for="props.id" :class="['input__label', { 'input__label_required': required }]">{{ props.label }}</label>
+            <label :for="props.id" :class="['input__label', { input__label_required: required }]">{{ props.label }}</label>
             <input
                 :id="props.id"
                 v-model="model"
@@ -13,7 +13,7 @@
                 autocomplete="off"
                 class="input__input"
                 @change="() => emit('change')"
-            >
+            />
             <div class="input__icon">
                 <slot name="trailing" />
             </div>
@@ -44,106 +44,105 @@ const hasError = computed((): boolean => {
 </script>
 
 <style scoped lang="scss">
-    .input {
+.input {
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+    width: 100%;
+
+    &__wrapper {
+        position: relative;
         display: flex;
         flex-direction: column;
-        gap: 6px;
+        gap: 3px;
         width: 100%;
+        height: 60px;
+        padding: 10px 16px 16px 20px;
+        border-radius: 18px;
+        border: 1px solid var(--input-border);
+        background-color: var(--input-bg);
 
-        &__wrapper {
-            position: relative;
-            display: flex;
-            flex-direction: column;
-            gap: 3px;
+        transition: border-color 0.2s ease-in-out;
+
+        &:has(> input:focus:not([readonly])) {
+            border: 1px solid var(--input-border-focused);
+        }
+
+        @include mobile {
+            gap: 6px;
+            padding: 12px 16px;
+        }
+    }
+
+    &__icon {
+        position: absolute;
+        right: 16px;
+        top: 0;
+        bottom: 0;
+        display: flex;
+        align-items: center;
+        min-width: 16px;
+        width: 16px;
+        height: 18px;
+        margin: auto;
+        color: var(--input-icon);
+
+        svg {
+            object-fit: contain;
             width: 100%;
-            height: 60px;
-            padding: 10px 16px 16px 20px;
-            border-radius: 18px;
-            border: 1px solid var(--input-border);
-            background-color: var(--input-bg);
+            height: 100%;
+        }
+    }
 
-            transition: border-color 0.2s ease-in-out;
+    &__label {
+        @include mainFont(500, 12, var(--input-label));
 
-            &:has(> input:focus:not([readonly])) {
-                border: 1px solid var(--input-border-focused);
-            }
+        @include mobile {
+            @include mainFont(500, 10, var(--input-label));
+        }
+
+        &_required:after {
+            content: ' *';
+            color: var(--input-asterisk);
+        }
+    }
+
+    &__input {
+        width: calc(100% - 32px);
+        outline: none;
+        border: none;
+        background-color: transparent;
+
+        @include mainFont(600, 14, var(--text-primary));
+
+        @include mobile {
+            @include mainFont(600, 12, var(--text-primary));
+        }
+
+        &::placeholder {
+            @include mainFont(500, 14, var(--input-icon), 1, italic);
 
             @include mobile {
-                gap: 6px;
-                padding: 12px 16px;
-            }
-        }
-
-        &__icon {
-            position: absolute;
-            right: 16px;
-            top: 0;
-            bottom: 0;
-            display: flex;
-            align-items: center;
-            min-width: 16px;
-            width: 16px;
-            height: 18px;
-            margin: auto;
-            color: var(--input-icon);
-
-            svg {
-                object-fit: contain;
-                width: 100%;
-                height: 100%;
-            }
-        }
-
-        &__label {
-            @include mainFont(500, 12, var(--input-label));
-
-            @include mobile {
-                @include mainFont(500, 10, var(--input-label));
-            }
-
-            &_required:after {
-                content: ' *';
-                color: var(--input-asterisk);
-            }
-        }
-
-        &__input {
-            width: calc(100% - 32px);
-            outline: none;
-            border: none;
-            background-color: transparent;
-
-            @include mainFont(600, 14, var(--text-primary));
-
-            @include mobile {
-                @include mainFont(600, 12, var(--text-primary));
-            }
-
-            &::placeholder {
-                @include mainFont(500, 14, var(--input-icon), 1, italic);
-
-                @include mobile {
-                    @include mainFont(500, 12, var(--input-icon), 1, italic);
-                }
-            }
-        }
-
-        &__error {
-             @include mainFont(500, 12, var(--input-invalid));
-        }
-
-        &_invalid {
-            border-color: var(--input-border);
-
-            .input__wrapper {
-                border-color: var(--input-invalid);
-            }
-
-            .input__icon,
-            .input__label
-            {
-                color: var(--input-invalid);
+                @include mainFont(500, 12, var(--input-icon), 1, italic);
             }
         }
     }
+
+    &__error {
+        @include mainFont(500, 12, var(--input-invalid));
+    }
+
+    &_invalid {
+        border-color: var(--input-border);
+
+        .input__wrapper {
+            border-color: var(--input-invalid);
+        }
+
+        .input__icon,
+        .input__label {
+            color: var(--input-invalid);
+        }
+    }
+}
 </style>
---
+++ widgets/modal/auth/index.vue
@@ -0,0 +1,161 @@
+<template>
+    <UiModal v-model="model">
+        <div class="auth-modal" :class="`auth-modal_${state}`">
+            <section class="auth-modal__section auth-modal__section_signup">
+                <h2 class="auth-modal__title">
+                    <span class="auth-modal__title_active">Регистрация</span> / <span @click="state = 'signin'">Вход</span>
+                </h2>
+                <div class="auth-modal__form">
+                    <div class="auth-modal__block">
+                        <h3 class="auth-modal__subtitle">Заполните обязательные поля</h3>
+                        <UiInput
+                            id="email"
+                            v-model="form.email"
+                            :error="errors.email"
+                            label="E-mail"
+                            placeholder="Введите почту"
+                            type="text"
+                            required
+                        >
+                            <template #trailing>
+                                <IconEnvelope />
+                            </template>
+                        </UiInput>
+                        <UiInput
+                            id="password"
+                            v-model="form.password"
+                            :error="errors.password"
+                            label="Password"
+                            placeholder="Введите пароль"
+                            type="password"
+                            required
+                        >
+                            <template #trailing>
+                                <IconLock />
+                            </template>
+                        </UiInput>
+                    </div>
+                    <div class="auth-modal__block">
+                        <h3 class="auth-modal__subtitle">Дополнительная информация</h3>
+                        <UiInput
+                            id="username"
+                            v-model="form.username"
+                            :error="errors.username"
+                            label="Username"
+                            placeholder="Введите никнейм"
+                            type="text"
+                        >
+                            <template #trailing>
+                                <IconAstronaut />
+                            </template>
+                        </UiInput>
+                        <div class="auth-modal__inputs">
+                            <UiInput
+                                id="firstname"
+                                v-model="form.firstname"
+                                :error="errors.firstname"
+                                label="Firstname"
+                                placeholder="Ваше имя"
+                                theme="outlined"
+                                type="text"
+                            />
+                            <UiInput
+                                id="lastname"
+                                v-model="form.lastname"
+                                :error="errors.lastname"
+                                label="Lastname"
+                                placeholder="...и фамилия"
+                                theme="outlined"
+                                type="text"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="auth-modal__footer">
+                    <UiButton>Зарегистрироваться</UiButton>
+                    <p class="auth-modal__policy">
+                        Данный сайт защищен reCAPTCHA с соответствующей <NuxtLink>политикой конфиденциальности Google</NuxtLink>
+                    </p>
+                </div>
+            </section>
+            <section class="auth-modal__section auth-modal__section_signin">
+                <h2 class="auth-modal__title">
+                    <span class="auth-modal__title_active">Вход</span> / <span @click="state = 'signup'">Регистрация</span>
+                </h2>
+                <div class="auth-modal__form auth-modal__form_signin">
+                    <div class="auth-modal__block">
+                        <h3 class="auth-modal__subtitle">Введите почту и пароль</h3>
+                        <UiInput
+                            id="email"
+                            v-model="form.email"
+                            :error="errors.email"
+                            label="E-mail"
+                            placeholder="Введите почту"
+                            type="text"
+                            required
+                        >
+                            <template #trailing>
+                                <IconEnvelope />
+                            </template>
+                        </UiInput>
+                        <UiInput
+                            id="password"
+                            v-model="form.password"
+                            :error="errors.password"
+                            label="Password"
+                            placeholder="Введите пароль"
+                            type="password"
+                            required
+                        >
+                            <template #trailing>
+                                <IconLock />
+                            </template>
+                        </UiInput>
+                    </div>
+                </div>
+                <div class="auth-modal__footer">
+                    <UiButton theme="blue-outline">Войти</UiButton>
+                    <p class="auth-modal__policy">
+                        Данный сайт защищен reCAPTCHA с соответствующей <NuxtLink>политикой конфиденциальности Google</NuxtLink>
+                    </p>
+                </div>
+            </section>
+            <section class="auth-modal__section auth-modal__section_image">
+                <NuxtImg src="/images/modals/auth/sign-up.png" class="auth-modal__image auth-modal__image_signup" :class="`auth-modal__image_`"/>
+                <NuxtImg src="/images/modals/auth/sign-in.png" class="auth-modal__image auth-modal__image_signin" :class="`auth-modal__image_${state}`"/>
+            </section>
+        </div>
+    </UiModal>
+</template>
+
+<script setup lang="ts">
+const model = defineModel<boolean>();
+
+const state = ref<string>('signup');
+
+type Form = {
+    username: string;
+    firstname: string;
+    lastname: string;
+    email: string;
+    password: string;
+};
+
+const form: Form = reactive({
+    username: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+});
+
+const errors = reactive({
+    username: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+});
+</script>
+
+<style scoped lang="scss" src="./auth-modal.styles.scss"></style>
