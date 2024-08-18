d55deba7724f84886dbdf4613f428f94a3f35398
Maxim Koshkin <humblerat@mail.ru>
2024-08-19 01:21:34 +0700

    init about, support & faq pages

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+    <NuxtLayout>
+        <div class="about">about</div>
+    </NuxtLayout>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
---
+++ pages/support.vue
@@ -0,0 +1,9 @@
+<template>
+    <NuxtLayout>
+        <div class="support">support</div>
+    </NuxtLayout>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
