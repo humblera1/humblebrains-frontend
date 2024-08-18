14c6e6cc110a4a0434f1037840d514037cb49577
Maxim Koshkin <humblerat@mail.ru>
2024-08-19 01:21:05 +0700

    update layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,9 @@
     <div class="app">
         <div class="app__page">
             <div class="app__inner">
-                <div class="app__header"></div>
+                <div class="app__header">
+                    <WidgetHeader />
+                </div>
                 <div class="app__content">
                     <aside class="app__sidebar"></aside>
                     <div class="content">
@@ -24,6 +26,10 @@
     min-height: 100dvh;
     height: 100dvh;
 
+    @include tablet {
+        grid-template-columns: 100vw;
+    }
+
     &__page {
         display: flex;
         justify-content: flex-end;
@@ -32,9 +38,15 @@
         border-radius: 0 48px 48px 0;
 
         @include boxShadow(4, -4, 25, 0, rgba(55, 64, 109, 0.1));
+
+        @include tablet {
+            padding: 15px;
+            border-radius: 0;
+        }
     }
 
     &__inner {
+        width: 100%;
         max-width: 1520px;
         height: 100%;
     }
@@ -42,6 +54,10 @@
     &__aside {
         width: 25vw;
         max-width: 400px;
+
+        @include tablet {
+            display: none;
+        }
     }
 }
 </style>
