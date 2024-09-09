28978bd65a4de2b03563af1e189145c83a0db530
Maxim Koshkin <humblerat@mail.ru>
2024-09-09 15:06:00 +0700

    added abstract interfaces to working with forms

---
+++ entities/interfaces/forms/AbstractFormFieldInterface.ts
@@ -0,0 +1,4 @@
+// An abstract interface for working with forms, showing that properties are always represented by a string
+export interface AbstractFormFieldInterface {
+    [key: string]: string | string[];
+}
