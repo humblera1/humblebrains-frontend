660cb3ebb2e881fd5a881f894eef6f931f309e17
Maxim Koshkin <humblerat@mail.ru>
2024-09-13 22:34:37 +0700

    updated menu modal component

---
+++ widgets/modal/menu/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <UiModal position="bottom" without-close-btn>
+        <div class="menu-modal">
+            <WidgetSidebar />
+            <div class="menu-modal__controls">
+                <WidgetLanguageSelect />
+                <WidgetColorMode />
+            </div>
+        </div>
+    </UiModal>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss" src="./menu-modal.styles.scss"></style>
