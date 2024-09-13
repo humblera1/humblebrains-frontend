4aa924e23d4b51f7435d44686221434bd8bbeaad
Maxim Koshkin <humblerat@mail.ru>
2024-09-13 22:32:39 +0700

    header modals (need to fix mobile version)

--- widgets/header/index.vue
+++ widgets/header/index.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="header">
         <div class="header__logo"></div>
-        <div class="header__bars">
-            <IconBars />
+        <div
+            :class="['header__bars', { header__bars_active: isSpecificWindowOpen(WidgetModalMenu) }]"
+            @click="toggleMenuModal(WidgetModalMenu)"
+        >
+            <Transition>
+                <IconCross v-if="isSpecificWindowOpen(WidgetModalMenu)" />
+                <IconBars v-else />
+            </Transition>
         </div>
         <div class="header__content">
             <div class="header__menu">
@@ -31,113 +37,29 @@
             <IconTrophy />
         </div>
     </div>
-    <WidgetModalAuth v-model:show="showAuthModal" v-model:state="authState" />
 </template>
 
 <script setup lang="ts">
-const showAuthModal = ref<boolean>(false);
-const authState = ref<string>('signin');
+import { WidgetModalAuth, WidgetModalMenu } from '#components';
+
+const authState = useState('authState');
+
+const { openModal } = useHumbleModal();
+const { toggleModal: toggleMenuModal, isSpecificWindowOpen } = useHumbleModal({ withBackdrop: false });
 
 const openSignin = () => {
     authState.value = 'signin';
-    showAuthModal.value = true;
+    openAuthModal();
 };
 
 const openSignup = () => {
     authState.value = 'signup';
-    showAuthModal.value = true;
+    openAuthModal();
+};
+
+const openAuthModal = () => {
+    openModal(WidgetModalAuth);
 };
 </script>
 
-<style scoped lang="scss">
-.header {
-    gap: 16px;
-    display: flex;
-    justify-items: center;
-    align-items: center;
-    width: 100%;
-    padding: 0 64px;
-
-    @include small-desktop {
-        padding: 0 36px;
-    }
-
-    @include tablet {
-        padding: 0 0 0 50px;
-    }
-
-    &__content {
-        display: flex;
-        justify-content: space-between;
-        gap: 32px;
-        width: 100%;
-
-        @include mobile {
-            justify-content: flex-end;
-        }
-    }
-
-    &__menu {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex: 1;
-
-        @include mobile {
-            display: none;
-        }
-    }
-
-    &__controls {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        gap: 24px;
-        flex: 1;
-
-        @include small-desktop {
-            flex: unset;
-        }
-
-        @include tablet {
-            display: none;
-        }
-    }
-
-    &__buttons {
-        display: flex;
-        gap: 16px;
-    }
-
-    &__signup {
-        @include small-desktop {
-            display: none;
-        }
-    }
-
-    &__bars,
-    &__results {
-        display: none;
-        justify-content: center;
-        align-items: center;
-        min-width: 40px;
-        width: 40px;
-        height: 40px;
-        border-radius: 12px;
-
-        @include tablet {
-            display: flex;
-        }
-    }
-
-    &__bars {
-        background-color: var(--white);
-
-        @include mainShadowMobile;
-    }
-
-    &__results {
-        background-color: var(--blue-badge);
-    }
-}
-</style>
+<style scoped lang="scss" src="./header.styles.scss"></style>
