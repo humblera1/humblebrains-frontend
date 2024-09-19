79501414e597e3eed7fe0c3fc2861ce6a7821988
Maxim Koshkin <humblerat@mail.ru>
2024-09-19 13:24:58 +0700

    added game page

---
+++ pages/games/[id].vue
@@ -0,0 +1,30 @@
+<template>
+    <NuxtLayout>
+        <component :is="component" v-if="component" />
+        <span v-else> loading... </span>
+    </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import type { Component } from 'vue';
+
+const route = useRoute();
+const router = useRouter();
+
+const component = shallowRef<Component>();
+
+const loadGameComponent = (gameName: string | string[]) => {
+    switch (gameName) {
+        case 'matrix':
+            component.value = defineAsyncComponent(() => import('@/widgets/game/matrix/index.vue'));
+            break;
+        default:
+            router.push('/404');
+    }
+};
+onMounted(() => {
+    loadGameComponent(route.params.id);
+});
+</script>
+
+<style scoped lang="scss"></style>
