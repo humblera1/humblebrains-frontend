7737352b83437df06a9779878ebe831c2f3c5ec6
Maxim Koshkin <humblerat@mail.ru>
2024-09-26 18:06:56 +0700

    added total time logic, bounded to timeline widget

--- stores/gameStore.ts
+++ stores/gameStore.ts
@@ -3,7 +3,67 @@ import { GameStateEnum } from '~/entities/enums/GameStateEnum';
 
 // Базовый стор, отвечающий за действия, характерные всем играм
 export const useGameStore = defineStore('gameStorage', () => {
+    /**
+     * Время на игру в секундах, скорее сего, будет приходить с бэка
+     */
+    const totalTime = ref<number>(90);
+
+    /**
+     * Идентификатор таймера, ответственного за уменьшение переменной totalTime
+     */
+    let totalTimerId: ReturnType<typeof setTimeout> | null = null;
+
     const gameState = ref<GameStateEnum>();
+
+    /** **************************************************************************************************************** Таймер totalTime */
+
+    /**
+     * Инициирует уменьшение переменной totalTime (на единицу каждую секунду)
+     */
+    const startTotalTimer = () => {
+        // check if timer is already started
+        if (isTotalTimerStarted()) {
+            return;
+        }
+
+        totalTimerId = setTimeout(function tick() {
+            if (totalTime.value <= 0) {
+                // @ts-ignore
+                clearTimeout(totalTimerId);
+                // todo:
+                console.log('Вышло время на игру');
+
+                return;
+            }
+            decreaseTotalTime();
+            totalTimerId = setTimeout(tick, 1000);
+        });
+    };
+
+    /**
+     * Останавливает уменьшение переменной totalTime
+     */
+    const stopTotalTimer = () => {
+        // @ts-ignore
+        clearTimeout(totalTimerId);
+        totalTimerId = null;
+    };
+
+    /**
+     * Уменьшает значение переменной totalTime на единицу
+     */
+    const decreaseTotalTime = () => {
+        totalTime.value--;
+    };
+
+    /**
+     * Проверяет, запущено ли уменьшение времени totalTime
+     */
+    const isTotalTimerStarted = () => {
+        return totalTimerId !== null;
+    };
+
+    /** *********************************************************************************************************************** Состояния */
 
     const setState = (state: GameStateEnum): void => {
         gameState.value = state;
@@ -87,5 +147,9 @@ export const useGameStore = defineStore('gameStorage', () => {
         isRoundFinishingState,
         isLevelFinishingState,
         isPromptState,
+
+        totalTime,
+        startTotalTimer,
+        stopTotalTimer,
     };
 });
--- widgets/game/ui/timeline/index.vue
+++ widgets/game/ui/timeline/index.vue
@@ -7,27 +7,13 @@
 </template>
 
 <script setup lang="ts">
-const time = ref<number>(90000);
-const timeLeft = ref<number>(90000);
+const gameStore = useGameStore();
+
+const totalTime: number = gameStore.totalTime;
 
 const timelineStyle = computed(() => {
-    return `width: ${timeLeft.value / time.value * 100}%`;
+    return `width: ${(gameStore.totalTime / totalTime) * 100}%`;
 });
-
-onMounted(() => {
-    const timerId = setTimeout(function decrease() {
-        if (timeLeft.value <= 0) {
-            clearTimeout(timerId);
-
-            return;
-        }
-
-        timeLeft.value -= 1000;
-        setTimeout(decrease, 1000);
-
-    }, 1000)
-})
-
 </script>
 
 <style scoped lang="scss" src="./game-ui-timeline.styles.scss"></style>
