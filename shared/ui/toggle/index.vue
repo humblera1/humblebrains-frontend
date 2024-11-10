706173cde7feace82038db265d43c6fcd631a7f6
Maxim Koshkin <humblerat@mail.ru>
2024-11-10 12:33:56 +0700

    added toggle component

---
+++ shared/ui/toggle/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div :class="['toggle', { toggle_on: model }]" @click="toggle">
+        <div class="toggle__switch"></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const model = defineModel<boolean>();
+const toggle = () => {
+    model.value = !model.value;
+};
+</script>
+
+<style scoped lang="scss" src="./toggle.styles.scss" />
