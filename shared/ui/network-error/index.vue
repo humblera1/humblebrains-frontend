ca8bc88bc18defb7682cf62b21892c1f006f9662
Maxim Koshkin <humblerat@mail.ru>
2024-11-21 14:47:32 +0700

    added network-error component

---
+++ shared/ui/network-error/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="network-error">
+        <div class="network-error__content">
+            <IconExclamationTriangle class="network-error__exclamation" />
+            <p class="network-error__message">
+                {{ $t('gameSavingError') }}
+            </p>
+            <UiButton @click="$emit('resend')">
+                <template #leading>
+                    <IconRepeat />
+                </template>
+                {{ $t('repeat') }}
+            </UiButton>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineEmits(['resend']);
+</script>
+
+<style scoped lang="scss" src="./network-error.styles.scss" />
