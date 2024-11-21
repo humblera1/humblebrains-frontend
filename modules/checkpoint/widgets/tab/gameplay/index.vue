8e17d01427dab9f45f6ab6192bc9721be8e4574f
Maxim Koshkin <humblerat@mail.ru>
2024-11-21 14:56:31 +0700

    updated gameplay tab

--- modules/checkpoint/widgets/tab/gameplay/index.vue
+++ modules/checkpoint/widgets/tab/gameplay/index.vue
@@ -1,12 +1,14 @@
 <template>
     <template v-if="page.currentTestComponent">
         <WidgetCheckpointUiLayout>
-            <template v-if="page.isPreviewTabSelected()">
-                <WidgetCheckpointTabGameplayInfo />
-            </template>
-            <template v-else-if="page.isGameplayTabSelected()">
-                <component :is="page.currentTestComponent.component" :key="page.currentTestComponent.name" />
-            </template>
+            <Transition name="slide" mode="out-in">
+                <template v-if="page.isPreviewTabSelected()">
+                    <WidgetCheckpointTabGameplayInfo />
+                </template>
+                <template v-else-if="page.isGameplayTabSelected()">
+                    <component :is="page.currentTestComponent.component" :key="page.currentTestComponent.name" />
+                </template>
+            </Transition>
         </WidgetCheckpointUiLayout>
     </template>
     <UiInDevelopment v-else />
@@ -18,4 +20,4 @@ import { useCheckpointPageStore } from '~/modules/checkpoint/stores/checkpointPa
 const page = useCheckpointPageStore();
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss" src="./gameplay.styles.scss" />
