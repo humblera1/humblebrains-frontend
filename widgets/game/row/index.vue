1666eee0bf517cb50d573141ad3c033d2ca4cd21
Maxim Koshkin <humblerat@mail.ru>
2025-02-05 01:05:28 +0700

    updated games row widget

--- widgets/game/row/index.vue
+++ widgets/game/row/index.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="row">
+    <div class="row" :class="{ row_full: !showArrows }">
         <Swiper
+            ref="swiperRef"
             :modules="[Pagination, Navigation, Mousewheel]"
             :slides-per-view="'auto'"
             :space-between="16"
@@ -16,15 +17,16 @@
                 prevEl: '.row__arrow_prev',
                 disabledClass: 'row__arrow_disabled',
             }"
+            @swiper="onSwiperInit"
         >
             <SwiperSlide v-for="(game, idx) in games" :key="idx">
                 <WidgetGameSnippet :game="game" :style="{ zIndex: games.length - idx }" />
             </SwiperSlide>
         </Swiper>
-        <div v-show="games?.length >= 1" class="row__arrow row__arrow_prev">
+        <div v-show="showArrows && games?.length >= 1" class="row__arrow row__arrow_prev">
             <IconChevron />
         </div>
-        <div v-show="games?.length >= 1" class="row__arrow row__arrow_next">
+        <div v-show="showArrows && games?.length >= 1" class="row__arrow row__arrow_next">
             <IconChevron />
         </div>
     </div>
@@ -33,12 +35,30 @@
 <script setup lang="ts">
 import { Mousewheel, Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/vue';
+import type { Swiper as SwiperType } from 'swiper/types';
 import type { GameRowProps } from '~/widgets/game/row/game-row.types';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 defineProps<GameRowProps>();
+
+const swiperRef = ref<SwiperType | null>(null);
+const showArrows = ref(true);
+
+const updateArrowsVisibility = () => {
+    if (!swiperRef.value) return;
+
+    showArrows.value = !(swiperRef.value.isBeginning && swiperRef.value.isEnd);
+};
+
+const onSwiperInit = (swiper: SwiperType) => {
+    swiperRef.value = swiper;
+};
+
+onMounted(() => {
+    updateArrowsVisibility();
+});
 </script>
 
 <style scoped lang="scss" src="./game-row.styles.scss" />
